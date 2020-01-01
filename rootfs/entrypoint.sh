dce4415ac9466818cf7c20d76593ffc1d4009146
ZalgoNoise <zalgo.noise@gmail.com>
2020-01-01 20:04:15 +0000

    [Docker OpenVPN] Rectified issues with entrypoint.sh where the initpki script was being called upon looking for the wrong file. Rectified to crl.pem instead

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -9,23 +9,26 @@ _genclient(){
      bash /usr/local/bin/easyrsa build-client-full $OVPN_CLIENT nopass \
      && bash /usr/local/bin/ovpn_getclient $OVPN_CLIENT > $_datapath/clients/$OVPN_CLIENT.ovpn
 }
-# Fetch needed environment variables
-
-if [ -z $OVPN_SERVER ]
-then echo -n "Define your public server IP (i.e.: 255.255.255.255): "
-     read OVPN_SERVER
-fi
-
-if [ -z $OVPN_SUBNET ]
-then echo -n "Define your custom subnet (i.e.: 10.8.0.0): "
-     read OVPN_SUBNET
-fi
 
 
 # Checking if files already exist
 # Setting up server
 if ! [ -f $_datapath/openvpn.conf ] || ! [ -f $_datapath/ovpn_env.sh ] || ! [ -d $_datapath/ccd ]
-then bash /usr/local/bin/ovpn_genconfig \
+then
+     # Fetch needed environment variables
+
+     if [ -z $OVPN_SERVER ]
+     then echo -n "Define your public server IP (i.e.: 255.255.255.255): "
+         read OVPN_SERVER
+     fi
+
+     if [ -z $OVPN_SUBNET ]
+     then echo -n "Define your custom subnet (i.e.: 10.8.0.0): "
+         read OVPN_SUBNET
+     fi
+
+
+     bash /usr/local/bin/ovpn_genconfig \
      -u udp://${OVPN_SERVER} \
      -p "redirect-gateway def1 bypass-dhcp" \
      -e "sndbuf 0" \
@@ -53,7 +56,7 @@ fi
 
 # Setting up keys
 
-if ! [ -d $_datapath/pki ]
+if ! [ -f $_datapath/crl.pem ]
 then bash usr/local/bin/ovpn_initpki
 fi
 
